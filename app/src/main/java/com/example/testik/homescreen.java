66963bc7f2b9a3fae2dee1f6a1cc0a7e572e11e2
kristina <kristinachetverg@gmail.com>
2024-04-03 12:53:55 +0700

    создан проект

---
+++ app/src/main/java/com/example/testik/homescreen.java
@@ -0,0 +1,14 @@
+package com.example.testik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homescreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+    }
+}
---
+++ app/src/main/java/com/example/testik/signIn.java
@@ -0,0 +1,14 @@
+package com.example.testik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
